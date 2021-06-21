42bfa1b91e895bd8b520e5add7936c96c8db6297
Exr0n <mail@exr0n.com>
2021-06-21 11:17:26 -0700

    data loading complete

--- challenges-in-representation-learning-facial-expression-recognition-challenge/main.py
+++ challenges-in-representation-learning-facial-expression-recognition-challenge/main.py
@@ -7,28 +7,32 @@ from matplotlib import pyplot as plt
 import torch
 import torchvision
 from torchvision import transforms
+from tqdm import tqdm
 
 import csv
 
 
 def load_data():
     # NTFS: could probably use torchvision.dataloader but meh
-    data_classes_count = [0] * len(DATA_CLASSES)
     data = [[]] * len(DATA_CLASSES)
+    print('loading data...')
     with open(DATAPATH, 'r') as rf:
-        for line in csv.DictReader(rf):
+        lines = sum(1 for line in rf)
+    with open(DATAPATH, 'r') as rf:
+        for line in tqdm(csv.DictReader(rf), total=lines):
             cls = int(line['emotion']) # unsafe
-            data_classes_count[cls] += 1
-            img = torch.tensor([int(p) for p in line['pixels'].split()], dtype=torch.float32).reshape((48, 48))
-            img /= 256
-            print(img)
-            plt.imshow(img)
-            plt.show()
+            img = torch.tensor([int(p) for p in line['pixels'].split()],
+                    dtype=torch.float32).reshape((48, 48))
+            img /= 256                  # normalize
+            # print(img)
+            # plt.imshow(img)
+            # plt.show()
             data[cls].append(img)
 
+    print([(DATA_CLASSES[i], len(data[i])) for i in range(len(DATA_CLASSES))]) # TODO: dataset is extremely unbalanced
 
-    print(list(zip(DATA_CLASSES, data_classes_count))) # TODO: dataset is extremely unbalanced
+    return data
 
 if __name__ == '__main__':
     print(f'pytorch version is {torch.__version__}')
-    load_data()
+    data = load_data()
